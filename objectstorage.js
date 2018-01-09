4bb5a58efb71c1fd0f073eafb6682b22cf459e0d
Edison Cury <edisoncuryn@gmail.com>
2018-01-09 11:55:46 -0200

    0.1.2

--- objectstorage.js
+++ objectstorage.js
@@ -133,7 +133,7 @@ class ObjectStorage{
         })
     };
 
-    getFile(fileName){
+    getFile(fileName, path){
         return new Promise((resolve, reject) => {
             this.init().then((storage) => {
                 var client = storage.client;
